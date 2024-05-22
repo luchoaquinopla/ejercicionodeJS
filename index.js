211c5f34e30be428d8143cc241434067477b0a32
luchoaquinopla <luchoaquinopla@gmail.com>
2024-05-22 13:34:04 -0300

    "segundo commit"

--- index.js
+++ index.js
@@ -17,20 +17,20 @@ app.get('/', (req, res) => {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
-    <div class="container">
+    <div class="col-md-4">
     <form action="/" method="get">
-      <div class="col-md-4">
+      <div class="">
         <input type="email" class="form-control" name="floatingInput" id="floatingInput" placeholder="name@example.com">
       </div>
       <br>
-      <div class="col-md-4">
+      <div class="">
         <input type="password" class="form-control" name="floatingPassword" id="floatingPassword" placeholder="Password">
       </div>
       <br>
       <div class="d-grid gap-2 d-md-flex justify-content-md-end">
         <input class="btn btn-primary btn-xl" type="submit" value="Submit">
       </div>
-    </form>
+    </form>Ñ
   `;
 
   // Verificar si hay datos guardados en la URL
